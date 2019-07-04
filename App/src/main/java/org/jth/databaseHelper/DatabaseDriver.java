49a429f701a195e26adb4d2a57e4cc0e7bc9f31c
TylarZhu <zhuxingyuan123@gmail.com>
2019-07-04 09:21:47 -0400

    Fix the name of method

--- App/src/main/java/org/jth/databaseHelper/DatabaseDriver.java
+++ App/src/main/java/org/jth/databaseHelper/DatabaseDriver.java
@@ -8,11 +8,13 @@ public class DatabaseDriver {
     private static final String dbClassName = "com.mysql.cj.jdbc.Driver";
     private static final String CONNECTION = "jdbc:mysql://localhost:3306/CSCC43Pro" +
             "?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "aax020020";
 
 
 
     public static void main(String[] args) {
-        Connection connection = connectOrCreateDatabase();
+        Connection connection = connectingToDatabase();
     }
 
 
@@ -20,11 +22,12 @@ public class DatabaseDriver {
      * Connects to the database if it is already set up.
      * @return Connection to the database
      */
-    public static Connection connectOrCreateDatabase() {
+    public static Connection connectingToDatabase() {
+        System.out.println("Connecting to database...");
         Connection connection = null;
         try {
             Class.forName(dbClassName);
-            connection = DriverManager.getConnection(CONNECTION, "root", "aax020020");
+            connection = DriverManager.getConnection(CONNECTION, USER, PASSWORD);
             System.out.println("Connect successed!");
         } catch (Exception e) {
             System.out.println("Something went wrong with your connection! see below details: ");
